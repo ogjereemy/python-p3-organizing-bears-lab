79c77326ce127bec6c3b55052d1b0633d953b1a5
ogola <ogolajeremy601@gmail.com>
2024-06-03 16:08:25 +0300

    adds bear lab

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES 
+('Mr. Chocolate', 5, 'M', 'Brown', 'Chill', True),
+('Rowdy', 7, 'F', 'Purple', 'Rowdy', True),
+('Tabitha', 1, 'F', 'Yellow', 'Evil', False),
+('Sergeant Brown', 8, 'M', 'Brown', 'Relaxed', True),
+('Melissa', 13, 'F', 'Pink', 'Hungry', True),
+('Grinch', 20, 'M', 'Green', 'Sleepy', True),
+('Wendy', 40, 'F', 'Black', 'Wild', True),
+(NULL, 60, 'M', 'Green', 'Wild', True);
